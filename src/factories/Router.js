abcbde1de9c28c08bcb27b7147498c5c8fd1cc2f
Rik Hoffbauer <Rikhoffbauer@gmail.com>
2015-12-13 18:31:26 +0100

    implemented defaultRoute and notFoundRoute

--- src/factories/Router.js
+++ src/factories/Router.js
@@ -338,22 +338,11 @@ function constructGrapnelRouter(options = {}, router) {
 function addRoutesToGrapnelRouter(options, router) {
   options.routes = options.routes || {};
 
-  if (options.defaultRoute) {
-    const routeObj = options.routes[options.defaultRoute];
-
-    if (!routeObj) {
-      throw new Error(`Default route '${options.defaultRoute}' doesn't exist.`);
-    }
-
-    options.routes[''] = routeObj;
-    options.routes['/'] = routeObj;
-  }
-
   _.each(options.routes, (route, routeName) => {
-    _.merge(route, Router.routeDefaults, {
+    _.defaults(route, Router.routeDefaults, {
       route: routeName,
       router
-    }, route);
+    });
 
     const middleware = [
       policiesMiddlewareFactory(route),
@@ -365,6 +354,47 @@ function addRoutesToGrapnelRouter(options, router) {
 
     return route;
   });
+
+  addDefaultRouteToGrapnelRouter(options, router);
+  addNotFoundRouteToGrapnelRouter(options, router);
+}
+
+function addDefaultRouteToGrapnelRouter(options, router) {
+  const defaultRoute = options.defaultRoute;
+
+  if (defaultRoute) {
+    if (!options.routes[defaultRoute]) {
+      throw new Error(`Default route '${defaultRoute}' doesn't exist.`);
+    }
+
+    const defaultRouteMiddleware = redirectMiddlewareFactory(router, defaultRoute);
+
+    router.grapnel.get('/', defaultRouteMiddleware);
+    router.grapnel.get('', defaultRouteMiddleware);
+  }
+}
+
+function addNotFoundRouteToGrapnelRouter(options, router) {
+  const notFoundRoute = options.notFoundRoute;
+
+  if (notFoundRoute) {
+    if (!options.routes[notFoundRoute]) {
+      throw new Error(`Default route '${notFoundRoute}' doesn't exist.`);
+    }
+
+    const notFoundMiddleware = redirectMiddlewareFactory(router, options.notFoundRoute);
+
+    router.grapnel.get('/*', notFoundMiddleware);
+    router.grapnel.get('*', notFoundMiddleware);
+  }
+}
+
+function redirectMiddlewareFactory(router, route) {
+  return function redirectMiddleware(req, event) {
+    if (!event.parent()) {
+      router.redirect(route);
+    }
+  }
 }
 
 export default Router;
